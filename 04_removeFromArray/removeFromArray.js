cc96b3bbe248a0372fb353a00ee13719d0254ac7
mrtn <martinakopyan34@gmail.com>
2023-04-11 13:49:59 +0300

    add remove frome array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+		for (let j = 0; j < args.length; j++) {
+			// console.log(args[j])
+			if (arr.indexOf(args[j]) !== -1) {
+				// delete arr[arr.indexOf(args[j])];
+				arr.splice(arr.indexOf(args[j]), 1)
+			}
+		}
 
+		return arr;
 };
 
+// const removeFromArray = function(array, ...args) {
+//   return array.filter(val => !args.includes(val))
+// }
+
+// console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4))
+console.log(removeFromArray(["hey", 2, 3, "ho"], "hey", 3))
+
 // Do not edit below this line
-module.exports = removeFromArray;
+// module.exports = removeFromArray;

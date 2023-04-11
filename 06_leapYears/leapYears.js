fe3fb0545887b170d4f136585e652e9c173dd4dd
mrtn <martinakopyan34@gmail.com>
2023-04-11 20:18:11 +0300

    finish exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(leapYear) {
+	if (leapYear % 400 === 0) {
+		return true;
+	} else if (leapYear % 100 === 0) {
+		return false;
+	} else if (leapYear % 4 === 0) {
+		return true
+	} else {
+		return false;
+	}
 };
 
+console.log(leapYears(1600))
 // Do not edit below this line
 module.exports = leapYears;
